3f10130ac3aa6af138a6dcc78b78a5f3d7ceb9ca
Omkumar T R <101443243+OmkumarTR@users.noreply.github.com>
2022-06-08 18:16:20 +0530

    Added

---
+++ src/com/chainsys/projectA/beans/Customer.java
@@ -0,0 +1,28 @@
+package com.chainsys.projectA.beans;
+
+public class Customer {
+
+	private int id;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void print () {
+		System.out.println(this.getId());
+		System.out.println(this.getName());
+	}
+}
--- src/com/chainsys/projectA/startup/Main.java
+++ src/com/chainsys/projectA/startup/Main.java
@@ -1,10 +1,34 @@
 package com.chainsys.projectA.startup;
-
+import com.chainsys.projectA.beans.Customer;
+import com.chainsys.projectA.beans.Employee;
+import org.springframework.context.ApplicationContext;					// Parent
+import org.springframework.context.support.ClassPathXmlApplicationContext; // Child
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		testB();
 	}
 
+	public static void testA() {
+		
+		Employee emp = new Employee();
+		emp.setId(1);
+		emp.setName("Omkumar");
+		emp.print();
+	}
+	
+	public static void testB() {
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		
+		Employee emp = (Employee)ac.getBean("emp");
+		emp.setId(1);
+		emp.setName("Omkumar");
+		emp.print();
+		Customer c = ac.getBean(Customer.class);
+		c.setId(1);
+		c.setName("Virat Kohli");
+		c.print();
+	}
+	
+	
 }
